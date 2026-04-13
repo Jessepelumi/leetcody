736d9c425127a51da79b5986be9afbd64379c2be
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-04-13 22:45:55 +0100

    #1: Two Sum

---
+++ neetcode/001_two_sum.py
@@ -0,0 +1,24 @@
+# Problem: #1 Two Sum
+# Difficulty: Easy
+
+class Solution:
+    def two_sum(nums: list[int], target: int) -> list[int]:
+        seen = {}
+
+        for index, value in enumerate(nums):
+            comp = target - value
+
+            if comp in seen:
+                return [seen[comp], index]
+            
+            seen[value] = index
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+
+solution = Solution
+nums = [11, 21, 31, -41]
+target = -30
+result = solution.two_sum(nums, target)
+print(result)
