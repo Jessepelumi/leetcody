e719d4a5114f3dfdd5b8b00e8c3eb9dbf9f4fb8a
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-04-14 16:04:54 +0100

    #3: Longest Substring Without Repeating Characters

---
+++ neetcode/003_longest_substring.py
@@ -0,0 +1,28 @@
+# Problem: #3 Longest Substring Without Repeating Characters
+# Difficulty: Medium
+
+# Sliding window problem
+
+class Solution:
+    def longest_substring(s: str) -> int:
+        seen = set()
+        max_window = 0
+        left = 0
+
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+
+            window = right - left + 1
+            max_window = max(max_window, window)
+            seen.add(s[right])
+
+        return max_window
+            
+
+
+solution = Solution
+s = "abcabcbb"
+result = solution.longest_substring(s)
+print(result)
