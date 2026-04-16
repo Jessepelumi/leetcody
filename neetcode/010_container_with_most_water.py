f8903526dd55cf0eb281986886f8788cd250576d
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-04-16 16:42:33 +0100

    Problem: 11. Count Good Nodes in Binary Tree

---
+++ neetcode/010_container_with_most_water.py
@@ -0,0 +1,29 @@
+# Problem: 11. Count Good Nodes in Binary Tree
+# Difficulty: Medium
+
+class Solution:
+    def container_most_water(height: list[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_water = 0
+
+        while left < right:
+            con_height = min(height[left], height[right])
+            con_width = right - left
+
+            max_water = max(max_water, (con_height * con_width))
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_water
+    
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+solution = Solution
+height = [1,8,6,2,5,4,8,3,7]
+result = solution.container_most_water(height)
+print(result)
