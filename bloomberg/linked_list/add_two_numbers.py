cad2ff90727b75a08037f1b8de4cdc6bf115f32d
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-04-29 16:16:55 +0100

    bloomberg practice: add two numbers

--- bloomberg/linked_list/add_two_numbers.py
+++ bloomberg/linked_list/add_two_numbers.py
@@ -5,4 +5,26 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pass
+        dummy = ListNode()
+        current = dummy
+        p1, p2 = l1, l2
+        carry = 0
+
+        while p1 or p2 or carry:
+            v1 = p1.val if p1 else 0
+            v2 = p2.val if p2 else 0
+
+            total = v1 + v2 + carry
+            digit = total % 10
+            carry = total // 7
+
+            current.next = ListNode(digit)
+
+            current = current.next
+            if p1:
+                p1 = p1.next
+            if p2:
+                p2 = p2.next
+
+        return dummy.next
+    
