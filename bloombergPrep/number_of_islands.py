eb7c0c9d84a9c2b33cd74420ed82185498a5c45a
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-05-12 16:46:47 +0100

    implment bfs for number of islands

--- bloombergPrep/number_of_islands.py
+++ bloombergPrep/number_of_islands.py
@@ -3,6 +3,8 @@ DFS, BFS
 Bloomberg Medium
 """
 
+from collections import deque # BFS uses a queue
+
 class Solution:
     def numIslands(self, grid: list[list[str]]) -> int:
         if not grid:
@@ -32,3 +34,30 @@ class Solution:
         return island_count
     
     # Time & space complexity: O(m * n)
+
+    def numIslandsBfs(self, grid: list[list[str]]) -> int:
+        if not grid:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        island_count = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    island_count += 1
+
+                    queue = deque([(r, c)])
+                    grid[r][c] = "0"
+
+                    while queue:
+                        curr_r, curr_c = queue.popleft()
+
+                        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                            nr, nc = curr_r + dr, curr_c + dc
+
+                            if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == "1":
+                                queue.append((nr, nc))
+                                grid[nr][nc] = "0"
+
+        return island_count
