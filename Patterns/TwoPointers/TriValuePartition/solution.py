ffa41aa2b245eea1516fa24bbdf5fe11593a751f
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-08-28 13:43:13 +0100

    Implement solution using partitioning

--- Patterns/TwoPointers/TriValuePartition/solution.py
+++ Patterns/TwoPointers/TriValuePartition/solution.py
@@ -4,7 +4,7 @@
 """
 
 class MultiplePass:
-    def tri_value_partition(nums: list[int]) -> list[int]:
+    def tri_value_partition(self, nums: list[int]) -> list[int]:
         count_zero = 0
         count_one = 0
         count_two = 0
@@ -24,4 +24,35 @@ class MultiplePass:
         for i in range(count_zero + count_one, len(nums)):
             nums[i] = 2
 
-        return nums 
+        return nums
+
+"""
+nums = [2,0,2,1,1,0]
+=> partitions => [ 0s |  1s  |  Unknown  |  2s  ]
+                   ^     ^                  ^
+                  low   mid                high
+"""
+
+class Solution:
+    def tri_value_partition(self, nums: list[int]) -> list[int]:
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+
+        return nums
+
+# nums = [2, 0, 2, 1, 1, 0]
+# sol = Solution()
+# result = sol.tri_value_partition(nums)
+# print(result)
